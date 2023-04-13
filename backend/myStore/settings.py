06574fc9b3499ada9a535971c64060cfa9b95279
unknown <noamamir01gmail.com>
2023-04-13 08:30:56 +0300

    updated cors settings

--- backend/myStore/settings.py
+++ backend/myStore/settings.py
@@ -46,6 +46,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*', host]
 CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
                       'content-type', 'accept', 'origin', 'authorization')
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "https://mystore-angular.onrender.com",
     "http://localhost:4200",
